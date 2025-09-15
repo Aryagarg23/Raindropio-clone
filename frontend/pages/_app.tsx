9d2fcbcd635aae48b13402c16f889928b9fda150
AryaGarg23 <Aryagarg23@users.noreply.github.com>
2025-09-15 12:34:13 -0400

    god commit

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,6 +4,15 @@ import '../styles/globals.css';
 import '@fontsource/inter/index.css';
 import '@fontsource/nunito-sans/index.css';
 
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
 }
